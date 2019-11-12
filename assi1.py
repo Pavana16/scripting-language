f709748c936d13030591ad321a5a070ef829f6c6
Pavana16 <54942282+Pavana16@users.noreply.github.com>
2019-11-12 21:12:19 +0530

    Delete assi1.py

--- assi1.py
+++
@@ -1,6 +0,0 @@
-def changeString(password):
-	for i in range(len(password)):
-		if(i='a' || i= 'e' || i = 'i' || i= 'o' || i= 'u')
-			password.replace(i,password.upper(i)
-pas=input("enter the pasword:")
-changeString(pas)
